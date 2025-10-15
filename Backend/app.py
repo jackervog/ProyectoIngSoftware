9779fcbc2ae9bf5d9a0bfb782de4861f3f148b97
jackervog <jackadampalacios@gmail.com>
2025-10-15 13:16:36 -0600

    nuevo

--- Backend/app.py
+++ Backend/app.py
@@ -106,6 +106,7 @@ def eliminar_cuenta(idUsuarios):
 
 #----------------------------------------------------
 
+
 @app.route('/PaginaTi')
 def PaginaTi():
     return render_template('TI/PaginaTi.html')
@@ -113,14 +114,3 @@ def PaginaTi():
 if __name__ == '__main__':
     app.run(debug=True)
 
-    #----------------------
-
-    @app.route('/CRUDRol')
-    def crud_roles():
-    conn = conectar()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT idRoles, Rol FROM roles ORDER BY idRoles")
-    roles = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return render_template('TI/CRUDRol.html', roles=roles)
