ffbee48ceeddae6355efce0f2d7dde480b1bc83e
Js990 <jesus_v258@hotmail.com>
2025-10-16 11:19:00 -0600

    Pa k a jak le salga bien

--- Backend/database.py
+++ Backend/database.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlclient://Xico:xico12345678@host="pduiits.mysql.database.azure.com"/bdtproyecto'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 db = SQLAlchemy(app)
